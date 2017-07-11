bbc193259c49ef8241e858c02f51ee424ab70f7e
Paul Crane <paul@acheronsoft.com>
2017-07-11 02:40:24 -0500

    Add hasOnlySpaces function.

--- source/dstringutils/utils.d
+++ source/dstringutils/utils.d
@@ -2,6 +2,7 @@ module dstringutils.utils;
 
 import std.regex;
 import std.conv;
+import std.string;
 
 /**
 	Formats a number using commas. Example 1000 => 1,000.
@@ -46,4 +47,32 @@ unittest
 	assert(formatNumber(100) == "100");
 	assert(formatNumber(1000) == "1,000");
 	assert(formatNumber(1000000) == "1,000,000");
+}
+
+/**
+	Checks a string for the presense of only whitespace.
+
+	Params:
+		text = The string to check.
+
+	Returns:
+		True of the string only contains whitespaces false otherwise.
+*/
+bool hasOnlySpaces(const string text)
+{
+	return text.length == text.countchars(" ") ? true : false;
+}
+
+///
+unittest
+{
+	immutable string spaces = "   ";
+	immutable string spaces1 = "1   ";
+	immutable string spacesx = "x   ";
+	immutable string mixed = " xall   s";
+
+	assert(spaces.hasOnlySpaces);
+	assert(!spaces1.hasOnlySpaces);
+	assert(!spacesx.hasOnlySpaces);
+	assert(!mixed.hasOnlySpaces);
 }
