9fc5c496921dd4ec1bdc51c516005c9d4a1670a8
Paul Crane <paul@acheronsoft.com>
2017-07-11 02:31:53 -0500

    Add formatNumber functions.

--- source/dstringutils/utils.d
+++ source/dstringutils/utils.d
@@ -1,1 +1,49 @@
 module dstringutils.utils;
+
+import std.regex;
+import std.conv;
+
+/**
+	Formats a number using commas. Example 1000 => 1,000.
+
+	Params:
+		number = The number to format.
+
+	Returns:
+		The formatted number;
+*/
+string formatNumber(const string number)
+{
+	auto re = regex(r"(?<=\d)(?=(\d\d\d)+\b)","g");
+	return number.replaceAll(re, ",");
+}
+
+///
+unittest
+{
+	assert(formatNumber("100") == "100");
+	assert(formatNumber("1000") == "1,000");
+	assert(formatNumber("1000000") == "1,000,000");
+}
+
+/**
+	Formats a number using commas. Example 1000 => 1,000.
+
+	Params:
+		number = The number to format.
+
+	Returns:
+		The formatted number;
+*/
+string formatNumber(const size_t number)
+{
+	return number.to!string.formatNumber;
+}
+
+///
+unittest
+{
+	assert(formatNumber(100) == "100");
+	assert(formatNumber(1000) == "1,000");
+	assert(formatNumber(1000000) == "1,000,000");
+}
